e647887da4c2bdd691c0221fa6aea0322044b052
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-07-23 23:49:21 -0300

    - Writing your first smoke test with k6

--- etc/k6/get-products-smoke-test.js
+++ etc/k6/get-products-smoke-test.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  vus: 1000,
+  duration: '5s',
 };
 
 export default function() {
-  let res = http.get('https://quickpizza.grafana.com');
+  let res = http.get('http://localhost:8083/api/v1/products');
   check(res, { "status is 200": (res) => res.status === 200 });
   sleep(1);
 }
