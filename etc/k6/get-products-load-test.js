b879c2570ac06026741bce8c92b21b6b2d8efafa
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-07-25 08:47:17 -0300

    - Implementing Load Testing

---
+++ etc/k6/get-products-load-test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '5s', target: 100 }, // Target is the VUs
+    { duration: '5s', target: 300 },
+    { duration: '5s', target: 500 },
+    { duration: '5s', target: 1000 },
+    { duration: '5s', target: 300 },
+    { duration: '5s', target: 0 },
+  ],
+};
+
+export default function () {
+  let res = http.get('http://localhost:8083/api/v1/products');
+  check(res, { "status is 200": (res) => res.status === 200 });
+  sleep(1);
+}
