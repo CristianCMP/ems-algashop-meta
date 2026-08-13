178262b4adffdb320218f2fedb0f855b954008e5
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-08-13 08:48:29 -0300

    - Presentation layer for password change

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -5,7 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
 	CREATE DATABASE authorization_server;
-	CREATE DATABASE authorization_server_test;
 	CREATE DATABASE ordering_test;
 	CREATE DATABASE billing_test;
+	CREATE DATABASE authorization_server_test;
 EOSQL
